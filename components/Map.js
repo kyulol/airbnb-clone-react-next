b658bf4bcae051f9199afa8f058cfccaf81c8b6a
Kyulol <sivan.jeremie@gmail.com>
2021-11-01 09:38:25 +0100

    try to fix deploy to vercel

--- components/Map.js
+++ components/Map.js
@@ -1,68 +1,69 @@
-import ReactMapGL, {Marker, Popup} from 'react-map-gl';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
+import { useState } from "react";
 import { getCenter } from 'geolib';
-import { useState } from 'react';
- 
-function Map({searchResults}) {
 
-  const [selectedLocation, setSelectedLocation] = useState()
+function Map({ searchResults }) {
 
-  const coordinates = searchResults.map(result => ({
+// for the pop-up marker, passing the object
+const [selectedLocation, setSelectedLocation] = useState({})
+
+// Transform the search result into latitude and longitude for geolib
+// { latitude: 52.516172, longitude: 13.124567}
+const coordinates = searchResults.map(result => ({
     longitude: result.long,
-    latitude: result.lat,
-  }))
-  
-  const center = getCenter(coordinates);
- 
-  const [viewport, setViewport] = useState({
-    width: '100%',
-    height: '100%',
+    latitude: result.lat
+}))
+
+// Center of the searched results using geoLib
+const center = getCenter(coordinates)
+
+const [viewport, setViewport] = useState({
+    width: "100%",
+    height: "100%",
     latitude: center.latitude,
     longitude: center.longitude,
-    zoom: 11,
+    zoom: 11
+})
 
-  });
-  
-  return (
-    <ReactMapGL
-    {...viewport}
-    mapStyle='mapbox://styles/dje-dje/ckvezbddp2c7714m66zco8aa6'
-    mapboxApiAccessToken={process.env.mapbox_key}
-    onViewportChange={(viewport) => setViewport(viewport)}
-  >
-    {searchResults.map(result => (
-      <div key={result.long}>
-        <Marker
-          longitude={result.long}
-          latitude={result.lat}
-          offsetLeft={0}
-          offsetTop={0}
+    return (
+        <ReactMapGL
+            mapStyle="mapbox://styles/dje-dje/ckve2n2kq374r16s5igvdry54"
+            mapboxApiAccessToken={process.env.mapbox_key}
+            {...viewport}
+            onViewportChange={(nextViewport) => setViewport(nextViewport)}
         >
-          <p 
-          role="img"
-          aria-label="push pin"
-          onClick={() => setSelectedLocation(result)}
-          className="cursor-pointer text-2xl">📍</p>
-        </Marker>
+            {searchResults.map(result => (
+                <div key={result.long}>
+                    <Marker
+                        longitude={result.long}
+                        latitude={result.lat}
+                        offsetLeft={0}
+                        offsetTop={0}
+                    >
+                        <p onClick={() => setSelectedLocation(result)} className="cursor-pointer text-2xl">
+                          📍
+                        </p>
+                    </Marker>
 
-        {selectedLocation.long === result.long ? (
-            <Popup
-                onClose={() => setSelectedLocation({})}
-                closeOnClick={true}
-                latitude={result.lat}
-                longitude={result.long}
-            >
-                <div>
-                    {result.title}
+                    {/* Popup to be shown once we click on the marker */}
+                    {selectedLocation.long === result.long ? (
+                        <Popup
+                            onClose={() => setSelectedLocation({})}
+                            closeOnClick={true}
+                            latitude={result.lat}
+                            longitude={result.long}
+                        >
+                            <div>
+                                {result.title}
+                            </div>
+                        </Popup>
+                    ) : (
+                        false
+                    )}
                 </div>
-            </Popup>
-        ) : (
-            false
-        )}
-
-      </div>
-    ))}
-  </ReactMapGL>
-  );
+            ))}
+        </ReactMapGL>
+    )
 }
 
 export default Map
--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
     domains: ['links.papareact.com'],
   },
   env: {
-    mapbox_key: 'pk.eyJ1IjoiZGplLWRqZSIsImEiOiJja2Q5eTc4cm4waWJ3MnltaXVhd2E0dDFzIn0.sK9f8XI6ZWXIBjFlSo_yXg'
+    mapbox_key: 'pk.eyJ1IjoiZGplLWRqZSIsImEiOiJja3ZnZXF5a3owMXc1MnVwM2R3am9iYjc4In0.1NIXSYlmh7udAFU0znRBLQ'
   }
 }
